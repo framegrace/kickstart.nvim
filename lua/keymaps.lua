9199f4461ca610c9c3967b9ce979b2bbb7034d30
Marc Gràcia <mgbuddy@gmail.com>
2024-12-04 08:13:50 -0500

    FIxed repetitions added tab to move between buffers

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,1 +1,2 @@
-vim.api.nvim_set_keymap('n', '<C-I>', '0i', { noremap = false })
+vim.api.nvim_set_keymap('n', '<C-i>', '0i', { noremap = false })
+vim.api.nvim_set_keymap('n', '<Tab>', ':bn<CR>', { noremap = true })
